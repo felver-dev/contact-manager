c48c97a15e66628806495c6167d28d2aae873b89
felver-dev <felver.ngouala@gmail.com>
2025-07-03 14:34:23 +0000

    AddContact() done

--- internal/services/contact.service.go
+++ internal/services/contact.service.go
@@ -2,9 +2,12 @@ package services
 
 import (
 	"fmt"
+	"strings"
+	"time"
 
 	"github.com/felver-dev/contact-manager/internal/models"
 	"github.com/felver-dev/contact-manager/internal/storage"
+	"github.com/felver-dev/contact-manager/internal/validators"
 )
 
 type ContactService struct {
@@ -45,7 +48,38 @@ func (cs *ContactService) LoadContacts() error {
 	return nil
 }
 
-func (cs ContactService) SaveContacts() error {
+func (cs *ContactService) SaveContacts() error {
 
 	return cs.storage.Save(cs.contacts)
+}
+
+func (cs *ContactService) AddContact(nom, telephone, email string) error {
+	if !validators.ValiderTelephone(telephone) {
+		return fmt.Errorf("format de téléphone invalid")
+	}
+
+	if !validators.ValiderEmail(email) {
+		return fmt.Errorf("format d'email invalid")
+	}
+
+	for _, contact := range cs.contacts {
+		if strings.EqualFold(contact.Email, email) {
+			return fmt.Errorf("un contact avec cet email existe déjà (ID: %d)", contact.ID)
+		}
+	}
+
+	maintenant := time.Now()
+	nouveauContact := models.Contact{
+		ID:        cs.prochainID,
+		Nom:       nom,
+		Telephone: telephone,
+		Email:     email,
+		Cree:      maintenant,
+		Modifie:   maintenant,
+	}
+
+	cs.contacts = append(cs.contacts, nouveauContact)
+	cs.prochainID++
+
+	return cs.SaveContacts()
 }
